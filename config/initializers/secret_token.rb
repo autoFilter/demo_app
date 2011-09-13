d2cd5b91965461972374aa71e355cea617dbf726
autoFilter <mooFilter@gmail.com>
2011-09-13 17:13:39 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'a04516eb192d9aa14b353b873f66163aa501a4d0d72be92c5b0e4d9f29d40cf5484a99876780f2916d99c6e5f6a5bf20e601f221feda96ef6ca798e1a0ff03a8'
